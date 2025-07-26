2d533f53945d083c57973da304fa6728f42928ce
Rin Iwai <rin@eyrin.jp>
2025-07-27 04:02:42 +0900

    wip

--- main/src/server.ts
+++ main/src/server.ts
@@ -1,11 +1,19 @@
+import { resolve } from 'node:path';
+import { app } from 'electron';
+
 export async function startViteServer(): Promise<string> {
   const { createServer, preview } = await import('vite');
+
+  const config = {
+    root: resolve(app.getAppPath(), './renderer'),
+  };
   const server = await (process.env.NODE_ENV === 'development'
-    ? (await createServer()).listen()
-    : preview());
+    ? (await createServer(config)).listen()
+    : preview(config));
   const url = server.resolvedUrls?.local[0];
   if (!url) {
     throw new Error('failed to start dev server');
   }
+
   return url;
 }
