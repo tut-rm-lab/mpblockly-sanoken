79dd74a45d2509048e57476c0fc4197110e998fb
Suzu-Gears <47468734+Suzu-Gears@users.noreply.github.com>
2025-08-05 05:25:29 +0900

    モーターを動かせた

--- packages/app/src/blockly/blocks/pico_tracer/motor.ts
+++ packages/app/src/blockly/blocks/pico_tracer/motor.ts
@@ -13,7 +13,6 @@ const motorPorts: MotorPorts = {
     Motor4: { pin_A: "6", pin_B: "7" }
 };
 
-
 const readEncoderDegree = defineBlock({ type: 'mpblockly_read_encoder_degree' }, (type) => {
     Blockly.defineBlocksWithJsonArray([
         {
@@ -50,5 +49,145 @@ const readEncoderDegree = defineBlock({ type: 'mpblockly_read_encoder_degree' },
     };
 });
 
+const readMotorCurrent = defineBlock({ type: 'mpblockly_read_motor_current' }, (type) => {
+    Blockly.defineBlocksWithJsonArray([
+        {
+            type,
+            message0: '%1 の電流を読み取る(mA)',
+            args0: [
+                {
+                    type: 'field_dropdown',
+                    name: 'MOTOR_PORT',
+                    options: [
+                        ['Motor1', '0'],
+                        ['Motor2', '1'],
+                        ['Motor3', '2'],
+                        ['Motor4', '3'],
+                    ],
+                },
+            ],
+            output: 'Number',
+            colour: 15,
+            tooltip: 'モーターの電流を返します。(絶対値)',
+        },
+    ]);
+    pythonGenerator.forBlock[type] = (block, generator) => {
+        const port = block.getFieldValue('MOTOR_PORT');
 
-export default defineCategory({ name: 'モーター' }, [readEncoderDegree]);
+        generator.provideFunction_('mpblockly_import_thread', 'import _thread');
+        generator.provideFunction_('mpblockly_import_motor_control', 'import motor_control');
+        generator.provideFunction_(`mpblockly_motor_instance`, `_thread.start_new_thread(motor_control.uart_worker, ())`);
+        const code = `motor_control.get_motor_current(${port})`;
+        return [code, Order.FUNCTION_CALL];
+    };
+});
+
+const setMotorPower = defineBlock({ type: 'mpblockly_set_motor_power' }, (type) => {
+    Blockly.defineBlocksWithJsonArray([
+        {
+            type,
+            message0: '%1 の出力を %2 %に設定',
+            args0: [
+                {
+                    type: 'field_dropdown',
+                    name: 'MOTOR_PORT',
+                    options: [
+                        ['Motor1', '0'],
+                        ['Motor2', '1'],
+                        ['Motor3', '2'],
+                        ['Motor4', '3'],
+                    ],
+                },
+                {
+                    type: 'field_number',
+                    name: 'POWER',
+                    value: 0,
+                    min: -100,
+                    max: 100,
+                },
+            ],
+            previousStatement: null,
+            nextStatement: null,
+            colour: 15,
+            tooltip: '時計回りが正 -100%から100%',
+        },
+    ]);
+    pythonGenerator.forBlock[type] = (block, generator) => {
+        const port = block.getFieldValue('MOTOR_PORT');
+        const power = block.getFieldValue('POWER');
+
+        generator.provideFunction_('mpblockly_import_thread', 'import _thread');
+        generator.provideFunction_('mpblockly_import_motor_control', 'import motor_control');
+        generator.provideFunction_(`mpblockly_motor_instance`, `_thread.start_new_thread(motor_control.uart_worker, ())`);
+        return `motor_control.set_motor_command(${port}, ${power}, False)\n`;
+    };
+});
+
+const setMotorBrake = defineBlock({ type: 'mpblockly_set_motor_brake' }, (type) => {
+    Blockly.defineBlocksWithJsonArray([
+        {
+            type,
+            message0: '%1 をブレーキ',
+            args0: [
+                {
+                    type: 'field_dropdown',
+                    name: 'MOTOR_PORT',
+                    options: [
+                        ['Motor1', '0'],
+                        ['Motor2', '1'],
+                        ['Motor3', '2'],
+                        ['Motor4', '3'],
+                    ],
+                },
+            ],
+            previousStatement: null,
+            nextStatement: null,
+            colour: 15,
+            tooltip: '指定したモーターにブレーキをかけます。',
+        },
+    ]);
+    pythonGenerator.forBlock[type] = (block, generator) => {
+        const port = block.getFieldValue('MOTOR_PORT');
+
+        generator.provideFunction_('mpblockly_import_thread', 'import _thread');
+        generator.provideFunction_('mpblockly_import_motor_control', 'import motor_control');
+        generator.provideFunction_(`mpblockly_motor_instance`, `_thread.start_new_thread(motor_control.uart_worker, ())`);
+        return `motor_control.set_motor_command(${port}, 0, False)\n`;
+    };
+});
+
+const setMotorCoast = defineBlock({ type: 'mpblockly_set_motor_coast' }, (type) => {
+    Blockly.defineBlocksWithJsonArray([
+        {
+            type,
+            message0: '%1 をフリーにする',
+            args0: [
+                {
+                    type: 'field_dropdown',
+                    name: 'MOTOR_PORT',
+                    options: [
+                        ['Motor1', '0'],
+                        ['Motor2', '1'],
+                        ['Motor3', '2'],
+                        ['Motor4', '3'],
+                    ],
+                },
+            ],
+            previousStatement: null,
+            nextStatement: null,
+            colour: 15,
+            tooltip: '指定したモーターの電源を切り、フリーにします。',
+        },
+    ]);
+    pythonGenerator.forBlock[type] = (block, generator) => {
+        const port = block.getFieldValue('MOTOR_PORT');
+
+        generator.provideFunction_('mpblockly_import_thread', 'import _thread');
+        generator.provideFunction_('mpblockly_import_motor_control', 'import motor_control');
+        generator.provideFunction_(`mpblockly_motor_instance`, `_thread.start_new_thread(motor_control.uart_worker, ())`);
+        return `motor_control.set_motor_command(${port}, 0, True)\n`;
+    };
+});
+
+
+export default defineCategory({ name: 'モーター' }, [readEncoderDegree, readMotorCurrent, setMotorPower, setMotorBrake, setMotorCoast]);
